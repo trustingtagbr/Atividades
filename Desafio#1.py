12c7d5ba1c9613184d08654d544134790ec8132c
Trustingtag <trustingtagbr@gmail.com>
2026-05-11 18:08:57 -0300

    Att 7 e 8 e 3 desafios

---
+++ Desafio#1.py
@@ -0,0 +1,8 @@
+"CRIE UM SCRIPT PYTHON QUE LEIA O NOME DE UMA PESSOA E MOSTRE UMA MENSAGEM DE BOAS VINDAS"
+"De Acordo com o valor Digitado"
+
+
+Nome = input("Nome: ")
+
+print("BOM DIA!", Nome)
+
---
+++ Desafio#3.py
@@ -0,0 +1,4 @@
+A = int(input("Número A: "))
+B = int(input("Número B: "))
+
+print("Soma dos Números selecionados:", A + B)
