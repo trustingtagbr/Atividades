7ca771534d18ed1c5a0d82491c3ace899fd90257
Trustingtag <trustingtagbr@gmail.com>
2026-06-01 20:31:20 -0300

    última aula do Mundo 1 de Python

---
+++ Praticas/práticaAula11.py
@@ -0,0 +1,33 @@
+#cor do terminal
+#Pode ser utilizado para especificar algum resultado
+# \33[0,1,4,7 ;30 a 37;40 a 47m
+
+""" 
+0  None
+1  Bold
+4  Underline
+7  Negative
+
+TEXT
+
+30 ⚪ Branco/Cinza
+31 🔴 Vermelho
+32 🟢 Verde
+33 🟡 Amarelo
+34 🔵 Azul
+35 🟣 Roxo
+36 🔷 Ciano
+37 ⚪ Cinza claro
+
+BACK
+
+40 ⬛ Fundo Preto
+41 🟥 Fundo Vermelho
+42 🟩 Fundo Verde
+43 🟨 Fundo Amarelo
+44 🟦 Fundo Azul
+45 🟪 Fundo Roxo
+46 🟦 Fundo Ciano
+47 ⬜ Fundo Cinza/Branco"""
+
+print('\33[1;33mleonardoaraujollf@gmail.com')
